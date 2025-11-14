9abdf8d76933fae35748e247efe1d944dff078e1
Ronny Cedillo <rosue96@hotmail.com>
2025-11-14 04:35:50 -0500

    Arreglando el build para Render

--- src/main/java/com/panaderia/App.java
+++ src/main/java/com/panaderia/App.java
@@ -4,7 +4,7 @@ import com.panaderia.controllers.*; // Importamos todos los controladores
 import io.javalin.Javalin;
 import io.javalin.http.staticfiles.Location;
 
-public class Main {
+public class App {
     public static void main(String[] args) {
         Javalin app = Javalin.create(config -> {
             config.staticFiles.add("/public", Location.CLASSPATH);
